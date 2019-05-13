ea8d07b6c567d48e7a90fd2a45b5bd1fdd0df14b
lnsnow <48871726+lnsnow@users.noreply.github.com>
2019-05-13 20:47:08 +0800

    Update cha7.py

--- cha7.py
+++ cha7.py
@@ -1,7 +1,8 @@
-class Myclass(object):
+# 加法运算
+class Myclass(object):  
     def fun(x,y):
         return (x+y)
-def main():
+def main():  
     use_class = Myclass.fun(1, 2)
     print(use_class)
 if __name__ == "__main__":
