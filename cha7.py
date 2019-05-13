8b96a022993b09186e678766cf39b0b1bf0461e9
ljwft <49267186+ljwft@users.noreply.github.com>
2019-05-13 20:41:30 +0800

    Create cha7.py

---
+++ cha7.py
@@ -0,0 +1,13 @@
+class Myclass(object):
+    def fun(x,y):
+        return (x+y)
+def main():
+    use_class = Myclass.fun(1, 2)
+    print(use_class)
+if __name__ == "__main__":
+    main()
+
+
+
+
+
